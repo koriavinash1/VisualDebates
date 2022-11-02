94fbf07daf7609a7799ad02a38ef3dfce1da04c0
Avinash <avin.kori.re@gmail.com>
2022-11-03 02:03:36 +0530

    Update exps.sh

--- scripts/exps.sh
+++ scripts/exps.sh
@@ -1,8 +1,8 @@
-# ./slurm.sh MNIST 3 64 64 64
-# ./slurm.sh MNIST 4 64 64 64
-# ./slurm.sh MNIST 6 64 64 64
-# ./slurm.sh MNIST 10 64 64 64
-# ./slurm.sh MNIST 20 64 64 64
+./slurm.sh MNIST 3 64 64 64
+./slurm.sh MNIST 4 64 64 64
+./slurm.sh MNIST 6 64 64 64
+./slurm.sh MNIST 10 64 64 64
+./slurm.sh MNIST 20 64 64 64
 
 ./slurm.sh SHAPES 3 32 32 64
 ./slurm.sh SHAPES 4 32 32 64
@@ -11,22 +11,22 @@
 ./slurm.sh SHAPES 20 32 32 64
 
 
-# ./slurm.sh AFHQ 3 128 128 128
-# ./slurm.sh AFHQ 4 128 128 128
-# ./slurm.sh AFHQ 6 128 128 128
-# ./slurm.sh AFHQ 10 128 128 128
-# ./slurm.sh AFHQ 20 128 128 128
+./slurm.sh AFHQ 3 128 128 128
+./slurm.sh AFHQ 4 128 128 128
+./slurm.sh AFHQ 6 128 128 128
+./slurm.sh AFHQ 10 128 128 128
+./slurm.sh AFHQ 20 128 128 128
 
 
-# ./slurm.sh AFHQ 4 64 64 64
-# ./slurm.sh AFHQ 6 64 64 64
-# ./slurm.sh AFHQ 10 64 64 64
-# ./slurm.sh AFHQ 20 64 64 64
+./slurm.sh AFHQ 4 64 64 64
+./slurm.sh AFHQ 6 64 64 64
+./slurm.sh AFHQ 10 64 64 64
+./slurm.sh AFHQ 20 64 64 64
 
-# ./slurm.sh AFHQ 4 64 64 128
-# ./slurm.sh AFHQ 6 64 64 128
-# ./slurm.sh AFHQ 10 64 64 128
-# ./slurm.sh AFHQ 20 64 64 128
+./slurm.sh AFHQ 4 64 64 128
+./slurm.sh AFHQ 6 64 64 128
+./slurm.sh AFHQ 10 64 64 128
+./slurm.sh AFHQ 20 64 64 128
 
 
-# ./ploting.sh
+./ploting.sh
